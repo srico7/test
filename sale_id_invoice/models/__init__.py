b5f2a78ef6a1e00a5f5f14f84935509b9fe39d1b
Chathura Munasinghe <72515484+srico7@users.noreply.github.com>
2025-07-15 12:00:34 +0530

    Delete sale_id_invoice directory

--- sale_id_invoice/models/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from . import account_invoice
-from . import sale_order
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- sale_id_invoice/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from . import sale_make_invoice_advance
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
